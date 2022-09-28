742509b01c5681c1a93b1a7caf43dd57565ccb67
TheSeagullDev <TheSeagullDev@users.noreply.github.com>
2022-09-28 00:38:47 +0000

    deploy: af4a777793b3fd203304c0858b677284f3b30fd0

--- _nuxt/static/1664325492/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1664325492",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"2948ea9b"}}}}(null,"\u002F"));
